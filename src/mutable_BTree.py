467c445174c8d9250101cb42143c200876831c7f
jjjustin0717 <jjjustin0717@github.com>
2022-05-14 12:47:47 +0800

    Update mutable_BTree.py

--- src/mutable_BTree.py
+++ src/mutable_BTree.py
@@ -100,7 +100,7 @@ class BTree(Generic[T]):
     some of methods using the same idea
     """
 
-    def parent(self, value: T1) -> Union[BTNode, None]:
+    def parent(self, value: T1) -> Any:
         """
         Parent method is used in reduce function
         """
@@ -417,7 +417,7 @@ class BTree(Generic[T]):
 
     # According to my understanding, what this function
     # should return is the sum of two bt trees
-    def concat(self, bt1: 'BTNode', bt2: 'BTNode') -> Any:
+    def concat(self, bt1: BTNode[Any], bt2: BTNode[Any]) -> Any:
         """
         Sum of two bt trees
         """
@@ -426,7 +426,7 @@ class BTree(Generic[T]):
         if not bt2:
             return bt1
         # return the new BTree
-        new_root = BTNode(bt1.value + bt2.value)  # type: BTNode
+        new_root = BTNode(bt1.value + bt2.value)  # type: BTNode[Any]
         new_root.left = self.concat(bt1.left, bt2.left)
         new_root.right = self.concat(bt1.right, bt2.right)
         return new_root
