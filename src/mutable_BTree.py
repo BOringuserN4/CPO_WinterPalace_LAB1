09ee7341163e590d74479b3333458ddc34734e40
jjjustin0717 <jjjustin0717@github.com>
2022-05-14 16:10:38 +0800

    Update mutable_BTree.py

--- src/mutable_BTree.py
+++ src/mutable_BTree.py
@@ -6,7 +6,7 @@ T1 = TypeVar('T1', str, int, float)
 
 class BTNode(Generic[T]):
     def __init__(self, value: Any = None,
-                 left: 'BTNode' = None, right: 'BTNode' = None) -> None:
+                 left: 'BTNode' = None, right: 'BTNode' = None):
         """
         init the BTNode
         :param value: None
