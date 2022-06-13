fcf7567dac4083501961127d7f3f9ce8c53042d5
jjjustin0717 <jjjustin0717@github.com>
2022-06-13 11:06:50 +0800

    Update mutable_BTree_test.py

--- src/mutable_BTree_test.py
+++ src/mutable_BTree_test.py
@@ -12,14 +12,16 @@ import unittest
 
 from hypothesis import given
 import hypothesis.strategies as st
-
+from typing import Any, TypeVar, List, Generic
 from mutable_BTree import BTNode, BTree
 
+T = TypeVar('T')
 
-class TestMutableBTree(unittest.TestCase):
 
-    def test_add(self):
-        btree = BTree()
+class TestMutableBTree(unittest.TestCase, Generic[T]):
+
+    def test_add(self) -> None:
+        btree: BTree[T] = BTree()
         btree.add(0)
         self.assertEqual(btree.to_list(), [0])
         btree.add(1)
@@ -31,65 +33,65 @@ class TestMutableBTree(unittest.TestCase):
         btree.add(4)
         self.assertEqual(btree.to_list(), [3, 1, 4, 0, 2])
 
-    def test_set_element(self):
-        lst = [0, 1, 2]
-        btree = BTree()
+    def test_set_element(self) -> None:
+        lst: List[int] = [0, 1, 2]
+        btree: BTree[T] = BTree()
         btree.from_list(lst)
         btree.set_element(1, 3)
         self.assertEqual(btree.to_list(), [3, 1, 2])
 
-    def test_parent(self):
-        btree = BTree()
+    def test_parent(self) -> None:
+        btree: BTree[T] = BTree()
         for item in range(10):
             btree.add(item)
         self.assertIsNone(btree.parent(0))
         self.assertIsNotNone(btree.parent(1))
 
-    def test_remove(self):
-        btree = BTree()
+    def test_remove(self) -> None:
+        btree: BTree[T] = BTree()
         for i in range(10):
             btree.add(i)
         btree.remove(4)
         self.assertNotIn(btree.to_list(), [4])
 
-    def test_size(self):
-        btree = BTree()
+    def test_size(self) -> None:
+        btree: BTree[T] = BTree()
         for i in range(10):
             btree.add(i)
         self.assertEqual(btree.size(), 10)
 
-    def test_is_member(self):
+    def test_is_member(self) -> None:
         btree = BTree().from_list([0, 1, 2, 3, 4, 5])
         self.assertTrue(btree.is_member(2))
         self.assertTrue(btree.is_member(4))
         self.assertTrue(btree.is_member(1))
 
-    def test_from_list(self):
+    def test_from_list(self) -> None:
         lst = [0, 1, 2]
-        btree = BTree().from_list(lst)
-        btree2 = BTree(BTNode(0, BTNode(1), BTNode(2)))
+        btree: BTree[T] = BTree().from_list(lst)
+        btree2: BTree[T] = BTree(BTNode(0, BTNode(1), BTNode(2)))
         self.assertEqual(btree.to_list(), btree2.to_list())
 
-    def test_to_list(self):
+    def test_to_list(self) -> None:
         lst = [0, 1, 2, 3]
-        btree = BTree()
+        btree: BTree[T] = BTree()
         btree.from_list(lst)
         self.assertEqual(btree.to_list(), [3, 1, 0, 2])
 
-    def test_filter(self):
-        btree = BTree().from_list([0, 1, 'a', 2, 'b'])
+    def test_filter(self) -> None:
+        btree: BTree[T] = BTree().from_list([0, 1, 'a', 2, 'b'])
         self.assertEqual(btree.filter(), [2, 1, 0])
 
-    def test_map(self):
-        def f(x):
+    def test_map(self) -> None:
+        def f(x: int) -> int:
             return abs(x)
 
         '''
         What happens, if you pass `abs` as argument for `map` function?
         Can you provide data, where result is not determined?
         '''
-        lst = [0, 1, 2, 3, 4, 5]
-        btree = BTree()
+        lst: List[int] = [0, 1, 2, 3, 4, 5]
+        btree: BTree[T] = BTree()
         btree.from_list(lst)
         btree.map(f)
         self.assertEqual(btree.to_list(), [3, 1, 4, 0, 5, 2])
@@ -102,19 +104,19 @@ class TestMutableBTree(unittest.TestCase):
         whether result is not determined?
         When changed whether to where, i can't understand what that means.
         '''
-        lst = [0, -1, -2, -3, -4, -5]
-        btree = BTree()
+        lst: List[int] = [0, -1, -2, -3, -4, -5]
+        btree: BTree[T] = BTree()
         btree.from_list(lst)
         btree.map(f)
         self.assertEqual(btree.to_list(), [3, 1, 4, 0, 5, 2])
 
-    def test_reduce(self):
+    def test_reduce(self) -> None:
         # sum of empty btree
-        btree = BTree()
+        btree: BTree[T] = BTree()
         self.assertEqual(btree.reduce(lambda st, e: st + e, 0), 0)
 
         # sum of btree
-        btree = BTree()
+        btree: BTree[T] = BTree()
         btree.from_list([1, 2, 3])
         self.assertEqual(btree.reduce(lambda st, e: st + e, 0), 6)
 
@@ -129,44 +131,44 @@ class TestMutableBTree(unittest.TestCase):
         btree.from_list(e)
         self.assertEqual(btree.reduce(lambda st, _: st + 1, 0), btree.size())
 
-    def test_next(self):
+    def test_next(self) -> None:
         lst = [0, 1, 2, 3, 4, 5]
-        btree = BTree()
+        btree: BTree[T] = BTree()
         btree.from_list(lst)
 
         iteration = iter(btree)
 
         self.assertIsNotNone(next(iteration))
 
-    def test_concat(self):
-        lst = [0, 1, 2]
-        btree1 = BTree()
+    def test_concat(self) -> None:
+        lst: List[int] = [0, 1, 2]
+        btree1: BTree[T] = BTree()
         btree1.from_list(lst)
-        lst2 = [2, 4, 6]
-        btree2 = BTree()
+        lst2: List[int] = [2, 4, 6]
+        btree2: BTree[T] = BTree()
         btree2.from_list(lst2)
 
-        btree = BTree()
+        btree: BTree[T] = BTree()
         btree_root = btree.concat(btree1.root, btree2.root)
         self.assertEqual(BTree(btree_root).to_list(), [5, 2, 8])
 
     @given(st.lists(st.integers()))
-    def test_from_list_to_list_equality(self, a):
-        btree = BTree()
+    def test_from_list_to_list_equality(self, a) -> None:
+        btree: BTree[T] = BTree()
         btree.from_list(a)
         b = btree.to_list()
         self.assertCountEqual(a, b)
 
     @given(st.lists(st.integers()))
-    def test_python_len_and_list_size_equality(self, a):
-        btree = BTree()
+    def test_python_len_and_list_size_equality(self, a) -> None:
+        btree: BTree[T] = BTree()
         btree.from_list(a)
         self.assertEqual(btree.size(), len(a))
 
     @given(a=st.lists(st.integers()),
            b=st.lists(st.integers()),
            c=st.lists(st.integers()))
-    def test_monoid_properties(self, a, b, c):
+    def test_monoid_properties(self, a, b, c) -> None:
         """
         Associativity
         For all a, b and c in S,
@@ -175,11 +177,11 @@ class TestMutableBTree(unittest.TestCase):
         There exists an element 0 in S,
         the equations 0 + a = a + 0 = a holds.
         """
-        btree1 = BTree()
+        btree1: BTree[T] = BTree()
         btree1.from_list(a)
-        btree2 = BTree()
+        btree2: BTree[T] = BTree()
         btree2.from_list(b)
-        btree3 = BTree()
+        btree3: BTree[T] = BTree()
         btree3.from_list(c)
 
         # (a + b) + c = a + (b + c)
@@ -200,9 +202,9 @@ class TestMutableBTree(unittest.TestCase):
                                concat(btree4.empty(), btree1.root)).to_list()
                          )
 
-    def test_iter(self):
+    def test_iter(self) -> None:
         lst = [0, 1, 2, 3, 4, 5]
-        btree = BTree()
+        btree: BTree[T] = BTree()
         btree.from_list(lst)
 
         lst2 = []
@@ -228,7 +230,7 @@ class TestMutableBTree(unittest.TestCase):
         item = next(i1)  # -> 4
         self.assertEqual(item, 4)
 
-        btree2 = BTree()
+        btree2: BTree[T] = BTree()
         iteration = iter(btree2)
         self.assertRaises(StopIteration, lambda: next(iteration))
 
